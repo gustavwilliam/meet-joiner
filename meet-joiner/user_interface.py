dd381ebd499ea0da5797cd25f0b7ac81552e6746
Gustav Odinger <65498475+gustavwilliam@users.noreply.github.com>
2021-01-12 22:28:08 +0100

    Add module for user interface
    Has a 'pick' function for letting the user pick an event out of many,
    and returns the index of the chosen item

---
+++ meet-joiner/user_interface.py
@@ -0,0 +1,37 @@
+"""Contains all utilities for the user interface."""
+
+from typing import List
+from datetime import datetime
+
+from simple_term_menu import TerminalMenu
+
+from events import Event
+
+
+def _humanize_time(time: datetime) -> str:
+    """Converts a datetime object into a XX:YY format"""
+    return time.strftime("%H:%M")
+
+
+def _get_menu_item(event: Event, index: str) -> List[str]:
+    start_time = _humanize_time(event.start_time)
+    end_time = _humanize_time(event.end_time)
+    title = event.title
+
+    return f"[{index}] {start_time}–{end_time} {title}"
+
+
+def pick(events: List[Event], title: str) -> int:
+    """
+    Let's the user pick one of the provided options.
+
+    Returns the index of the chosen item.
+    """
+    menu_items = [_get_menu_item(event, i) for i, event in enumerate(events)]
+
+    menu = TerminalMenu(
+        menu_items,
+        title=title,
+    )
+
+    return menu.show()

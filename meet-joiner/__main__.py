9e20114cdc57a08dce9ad3f2282f93e98d243858
Gustav Odinger <65498475+gustavwilliam@users.noreply.github.com>
2021-01-12 23:48:00 +0100

    Add __main__ file for running module
    Prompts the user to connect to the meeting and connects them.

---
+++ meet-joiner/__main__.py
@@ -0,0 +1,37 @@
+"""Module with the public methods of the package."""
+
+from datetime import date
+from typing import List
+
+from api_caller import Calendar
+from auth import get_credentials
+from user_interface import pick, confirm
+from events import events_on_date, Event
+
+
+def pick_and_connect(events: List[Event]) -> None:
+    """
+    Allows the user to pick an event and opens it in the browser, if a meeting link exists.
+
+    If no meeting link is present, the user is asked if they want to try picking a different event.
+    """
+    event = pick(events, "Pick an event to connect to:")
+
+    try:
+        event.connect_to_meeting()
+        print("Connecting...")
+    except AttributeError as e:
+        if confirm(f"{e} Choose another event?"):
+            pick_and_connect(events)
+
+
+def main() -> None:
+    """Main function."""
+    calendar = Calendar(get_credentials())
+    events = events_on_date(date.today(), calendar)
+
+    pick_and_connect(events)
+
+
+if __name__ == "__main__":
+    main()

b1b49fc0c26e5dfcb94525abff4992c40fb376c6
Gustav Odinger <65498475+gustavwilliam@users.noreply.github.com>
2021-01-12 22:42:01 +0100

    Add function to retreive all events on a data

--- meet-joiner/events.py
+++ meet-joiner/events.py
@@ -1,6 +1,9 @@
 """Module for managing and storing Events."""
 
-from datetime import datetime
+from datetime import date, datetime, timedelta
+from typing import List
+
+from api_caller import Calendar
 
 
 class Event:
@@ -16,3 +19,19 @@ class Event:
             self.meeting_link = api_response["conferenceData"]["entryPoints"][0]["uri"]
         except KeyError:
             self.meeting_link = None
+
+
+def events_on_date(target: date, calendar: Calendar) -> List[Event]:
+    """
+    Get all events of a chosen day day.
+
+    Returns a list of Event objects.
+    """
+    tomorrow = target + timedelta(days=1)
+
+    return [
+        Event(event)
+        for event in calendar.events_in_time_range(min_date=target, max_date=tomorrow)[
+            "items"
+        ]
+    ]
